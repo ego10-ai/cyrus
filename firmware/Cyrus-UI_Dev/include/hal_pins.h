12699730b4603cd92b31a9c3d4d52c0e21c52edf
ego10-ai <bhaveshkk2001@gmail.com>
2026-04-29 04:31:41 +0530

    [200~change the currentState variable~)

--- firmware/Cyrus-UI_Dev/include/hal_pins.h
+++ firmware/Cyrus-UI_Dev/include/hal_pins.h
@@ -1,4 +1,9 @@
-// --- I2C Bus ---
+
+#ifndef HAL_PINS_H
+#define HAL_PINS_H
+
+// --- I2C Bus ---()(Devices ;- BQ25896, MAX17048, PCF85063A, XL9555, BME280, DRV2605, OLED, Keyboard )
+
 #define PIN_I2C_SDA       8
 #define PIN_I2C_SCL       9
 
@@ -12,6 +17,11 @@
 #define PIN_LORA_RST      14
 #define PIN_LORA_BUSY     21
 #define PIN_LORA_IRQ      47
+
+
+
+
+// in my schematics i uses IO1 and IO2 for LoRa, but in the E22-900M22S 
 
 // --- GPS (MAX-M10S) ---
 #define PIN_GPS_RX_ESP_TX 42  // ESP32 TX connects to GPS RX
@@ -35,4 +45,29 @@
 // --- Hardware Buttons ---
 #define PIN_BTN_DOWN      37
 #define PIN_BTN_UP        36
-#define PIN_BTN_SEL       35
+#define PIN_BTN_SEL       35    // labeled SCLT in schematics
+
+// XL9555 I/O Expander Pins Mappings
+// (these are not ESP32 pins,these are IO expender ports )
+//------------------------------------------------------
+#define EXP_PIN_GPS_EN   0   // Port 100-0: Turns the GPS LDO on/off
+#define EXP_PIN_LORA_EN  1   // Port 100-1: Turns the LoRa LDO on/off
+#define EXP_PIN_SD_EN    2   // Port 100-2: Turns the SD card LDO on/off
+#define EXP_PIN_LCD_RST  3   // Port 100-3: Resets the OLED display
+#define EXP_PIN_KEY_INT  4   // Port 100-4: Interrupt from the keyboard
+#define EXP_PIN_MOTOR_EN 5   // Port 100-5: turns vibration motor no/off
+#define EXP_PIN_KB_BL    6   // Port 100-6: Keyboard backlight control
+
+//------------------------------
+//I2C DEVICES Addresses
+//------------------------------
+#define I2C_ADDR_BQ25896   0x68
+#define I2C_ADDR_MAX17048  0x36
+#define I2C_ADDR_PCF85063  0x51
+#define I2C_ADDR_XL9555    0x20
+#define I2c_ADDR_BME280    0x76
+
+
+
+// notew for me to self: OLED was originally plan to used at 0x20 but the XL9555 is also at 0x20, 
+// so i change the OLED to 0x3C, and it works fine, but i forgot to update the define here, so i will do it later when i implement the OLED driver.
--- firmware/Cyrus-UI_Dev/src/main.cpp
+++ firmware/Cyrus-UI_Dev/src/main.cpp
@@ -1,27 +1,41 @@
+// 1.include area ---------=------------------------------
+
 #include <Arduino.h>
 #include <hal_pins.h>
 #include <UIManager.h>
+#include <OneButton.h>
 
 
 // decleeartioon for the tasks
+// 2.GLOBAL VARIABLES
+//-----------------------------------------
+
+
+
+
+//3. task declaration
 
 void taskUI(void *pvParameters);
 
 void taskRadioGPS(void *pvParameters);
 
+
+// 4.SETUP FUNCTION (Runs once on boot)
 void setup() {
   Serial.begin(115200);
+  delay(2000); 
   
+
 
   // UI task and sensor Task on core 1
   xTaskCreatePinnedToCore(
     taskUI,          // Task function
     "Task_UI",       // Name of the task 
-    8192,
+    8192,            // memory size in bytes
     NULL,
-    1,
+    1,               // priority
     NULL,
-    1
+    1                // core 1    
 
     );
 
@@ -31,22 +45,34 @@ void setup() {
 
     8192,
     NULL,
-    2,
+    2,           // Higher priority for communications
     NULL,
     0
     );
 }
 
+
+//- 5. standard loop -------
+/// @brief Main loop, not used as we are using FreeRTOS tasks, so we delete the default loop task to save resources
 void loop() {
   // free Rtos is handling the task scheduling
   vTaskDelete(NULL);
 }
 
-// --- task Implementiation ---
+// --- 6. task Implementiation ---
 
 void taskUI(void *pvParameters) {
+
+    // everything in this 'for' loop runs forever on core 1
   for(;;) {
-    // FMS logic, and OLED drawing will go here
+
+    // FMS logic, and OLED drawing 
+    case STATE_BOOT_LOGO:
+      Serial.println("Display: showing Boot Logo..............");
+      vTaskDelay(pdMS_TO_TICKS(2000));  // wqait for 2 sec
+      currentState = STATE_DASHBOARD; // changfe the state !
+      break;
+
     vTaskDelay(pdMS_TO_TICKS(50)); // Run at ~20hz
   }
   }
